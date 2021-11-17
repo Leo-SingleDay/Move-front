71a856f9b207fd3391f16f32e91890278e7536d5
Leo <guswhddlf11@gmail.com>
2021-11-17 14:10:12 +0900

    1117:14:10

--- src/views/Movies.vue
+++ src/views/Movies.vue
@@ -1,10 +1,41 @@
 <template>
-  <div></div>
+  <div>
+    <p 
+      v-for="movie in movies"
+      :key="movie.pk"  
+    >
+      {{movie.title}}
+    </p>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'movies',
+  data: function () {
+    return {
+      movies : [],
+    }
+  },
+  methods: {
+    getMovies: function() {
+      axios({
+        method: 'get',
+        url: 'http://127.0.0.1:8000/movies/',
+      })
+        .then(res => {
+          this.movies = res.data
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
+  },
+  created: function () {
+    this.getMovies()
+  }
 }
 </script>
 
