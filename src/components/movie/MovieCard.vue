2af55de6bff5dc0f7e0600a1e070f21c888ac999
송진섭 <sjs9238@gmail.com>
2021-11-18 17:55:19 +0900

    carousel 완성

--- src/components/movie/MovieCard.vue
+++ src/components/movie/MovieCard.vue
@@ -1,15 +1,85 @@
 <template>
   <div>
+    <b-carousel
+      class="carousel-height hc"
+      id="carousel-1"
+      v-model="slide"
+      :interval="4000"
+      controls
+      indicators
+      background="#ababab"
+      img-width="1024"
+      img-height="480"
+      style="text-shadow: 1px 1px 2px #333;"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+    >
+      <span 
+        v-for="movie in movies"
+        :key="movie.id"
+      >
+        
+        <b-carousel-slide
+          :img-src="movie.poster_path"
+        >
+          <!-- <b-button 
+            v-b-modal.modal-scrollable
+          >
+            <movie-card-detail :movie="movie"></movie-card-detail>
+            
+          </b-button> -->
 
+        
+        </b-carousel-slide>
+        
+        
+      </span>
+     </b-carousel>
+     
   </div>
+  
 </template>
 
 <script>
+// import MovieCardDetail from '@/components/movie/MovieCardDetail.vue'
+
 export default {
-  name: 'MovieCard'
+  name: 'MovieCard',
+  // components: {
+  //   MovieCardDetail
+  // },
+  data: function () {
+    return {
+      slide: 0,
+      sliding: null
+    }
+  },
+  props: {
+    movies: Object,
+  },
+  methods: {
+      onSlideStart() {
+        this.sliding = true
+      },
+      onSlideEnd() {
+        this.sliding = false
+      }
+  }
 }
 </script>
 
 <style>
+.carousel-height {
+   height:300px;
+   /* width: 20%; */
+ }
+.hc {
+  width:200px;
+  left:0;
+  right:0;
+  margin-left:auto;
+  margin-right:auto;
+}
+
 
 </style>
---
+++ src/components/movie/MovieCardDetail.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+
+    <b-modal id="modal-scrollable" scrollable :title="movie.title">
+      <p class="my-4">
+        {{ movie.overview }}
+      </p>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MovieCardDetail',
+  props: {
+    movie: Object
+  },
+
+}
+</script>
+
+<style>
+
+</style>
